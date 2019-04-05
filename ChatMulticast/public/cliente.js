f4408ec5234bde3614bb14c5aa3344e6a42e5367
Enrike Ramos <39656338+brokenerk@users.noreply.github.com>
2019-04-05 00:05:12 -0600

    avance usuarios

--- ChatMulticast/public/cliente.js
+++ ChatMulticast/public/cliente.js
@@ -1,30 +1,54 @@
 $(function () {
   var socket = io();
 
-  //emit -----> on
-  //Con emit enviamos, con on recibimos
-  socket.emit('chat message', 'user connected');
+  //Para obtener el nickname desde GET
+  function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+    results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+  } 
 
-  $('form').submit(function(e){
-    e.preventDefault(); // prevents page reloading
-    var msj = $('#m').val();
-    //Envia mensajes a otros clientes
-    socket.emit('chat message', msj);
-    $('#messages').append($('<li>').text(msj));
-    $('#m').val('');
-    console.log(msj);
-    return false;
+  var nickname = getParameterByName("nickname");
+  socket.emit("user connected", nickname);
+
+  //Envia mensajes
+  $("#enviarMensaje").click(function(e){
+    e.preventDefault();
+    var msj = $("#m").val();
+
+    if(msj != ""){
+      //Envia mensajes a otros clientes
+      socket.emit("chat message", msj);
+      $("#messages").append($("<li>").text(msj));
+      $("#m").val("");
+    }
   });
 
+  //Muestra los usuarios online
+  socket.on("display users", function(users){
+    $("#users").empty();
+    for(var i = 0; i < users.length; i++)
+      if(users[i] != nickname)
+        $("#users").append($("<li id= '" + users + "'>").text(users));
+    //console.log(users); 
+  })
+
+  //Agrega a los usuarios cuando se conecten
+  socket.on("user connected", function(data){
+     $("#users").append($("<li id= '" + data.nickname + "'>").text(data.nickname));
+     $("#messages").append($("<li>").text(data.numUsers + " users online"));
+  })
+
+  //Elimina el usuario desconectado
+  socket.on("user disconnected", function(nickname){
+    console.log("usuario " + nickname + " desconectado");
+    $("#" + nickname).remove();
+  })
+
   //Recibe del servidor los mensajes de otros clientes
-  socket.on('chat message', function(msg){
-    if(msg == 'user connected') {
-      console.log("New user");
-      $('#users').append($('<li>').text(msg));
-    }
-    else {
-      $('#messages').append($('<li>').text(msg));      
-    }
+  socket.on("chat message", function(data){
+    $("#messages").append($("<li>").text(data.nickname + ": " + data.msg));      
   });
   
 });
--- ChatMulticast/servidor.js
+++ ChatMulticast/servidor.js
@@ -1,29 +1,65 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var path = require('path')
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var path = require("path")
+var server = require("http").createServer(app);
+var io = require("socket.io")(server);
 
 //Construye el HTML del cliente
-app.use(express.static('public'));
-app.get('/', function(req, res){
-	res.sendFile('index.html');
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/public/login.html");
 });
+
+app.get('/chatGlobal', (req, res) => {
+  res.sendFile(__dirname + "/public/chatGlobal.html");
+});
+
+server.listen(3000, function(){
+	console.log("Servidor iniciado en: http://localhost:3000");
+});
+
+var numUsers = 0;
+var usersOnline = [];
+
+function sendAllUsersOnline(socket){
+	socket.broadcast.emit("display users", usersOnline);
+}
 
 //Escucha las peticiones de los clientes conectados
-io.on('connection', function(socket){
+io.on("connection", function(socket){
+	//Se envian los clientes online
+	sendAllUsersOnline(socket);
+
+	//Se conecta un cliente
+	socket.on("user connected", function(nickname){
+		socket.nickname = nickname;
+		numUsers++;
+		usersOnline.push(socket.nickname);
+
+		console.log(socket.nickname + " conectado");
+		socket.broadcast.emit("user connected", {
+			nickname: socket.nickname,
+			numUsers: numUsers
+		});
+
+		sendAllUsersOnline(socket);
+	})
+
 	//El cliente envia un mensaje
-	socket.on('chat message', function(msg){
+	socket.on("chat message", function(msg){
 		//El servidor envia el mensaje a todos los demas
-		socket.broadcast.emit('chat message', msg);
-	});
+		socket.broadcast.emit("chat message", {
+			nickname: socket.nickname,
+			msg: msg
+			});
+	})
+
 	//El cliente se desconecta
-	socket.on('disconnect', function(){
+	socket.on("disconnect", function(){
 		//El servidor notifica todos los demas
-	    socket.broadcast.emit('chat message', 'user disconnected');
-  	});
+		usersOnline.splice($.inArray(socket.nickname, usersOnline), 1);
+	    socket.broadcast.emit("user disconnected", socket.nickname);
+  	})
 });
 
-http.listen(3000, function(){
-	console.log('Servidor iniciado en: http://localhost:3000');
-});
