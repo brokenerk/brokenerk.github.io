106e807e7dedc01512cb51a183621a59b8c7f0be
Enrike Ramos <39656338+brokenerk@users.noreply.github.com>
2019-07-20 17:35:30 -0500

    index design ready

--- TWeb/CodigosClase/loginAJAX_1_20191/php/cerrarSesion.php
+++
@@ -1,7 +0,0 @@
-<?php
-	session_start();
-	//session_destroy();
-	$temp = $_REQUEST["nombreSesion"];
-	unset($_SESSION[$temp]);
-	header("location:../");
-?>
--- TWeb/CodigosClase/sesion2.php
+++
@@ -1,5 +0,0 @@
-<?php
-    session_start();
-    
-    echo "HOLA ".$_SESSION["boleta"];
-?>
